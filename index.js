95d088e8f54b15f662c6fde7cadeb05bb6c53b25
Javier González <46821629+FrancisJaGlez@users.noreply.github.com>
2021-10-20 21:58:28 -0500

    Primer avance

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const path = require("path")
+
+const bodyParser = require("body-parser")
+
+const Mongo = require("mongoose")
+//Monfo.connect...
+
+
+
+
+
+const Routes = require('./routes/carta')
+
+const app = express();
+
+
+//Configurar el servidor y que sepa que es un json
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use("/baraja",Routes);
+
+//Que la aplicación escuche peticiones
+Mongo.connect(`mongodb://localhost:27017/baraja`)
+  .then(app.listen(8080,()=>{
+    console.log("Aplicación web en línea localhost:8080")
+  })
+  .catch(err=>console.log(err))
